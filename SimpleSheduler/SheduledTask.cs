555417b682df8779cedf93028ca6d86a465ba9c2
Lehonti Ramos <lehonti@ramos>
2023-10-19 23:29:27 +0200

    Removed unused `set`ters in `SheduledTask`

--- SimpleSheduler/SheduledTask.cs
+++ SimpleSheduler/SheduledTask.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleSheduler
 {
-    public class SheduledTask
+    public sealed class SheduledTask
     {
-        public int TaskID { get; set; }
-        public string TaskName { get; set; }
-        public string TaskDescription { get; set; }
-        public bool IsFinished { get; set; }
-        public bool IsInProcess { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime ChangedDate { get; set; }
+        public int TaskID { get; }
+        public string TaskName { get; }
+        public string TaskDescription { get; }
+        public bool IsFinished { get; }
+        public bool IsInProcess { get; }
+        public DateTime CreatedDate { get; }
+        public DateTime ChangedDate { get; }
 
 
         public SheduledTask(int taskId = 0, 
