111786e126236d9060879783e55a67ac20253276
Vitalii Yelizarov <v196093@gmail.com>
2023-10-19 21:57:41 +0200

    Implemented data validation

--- SimpleSheduler/Validators.cs
+++ SimpleSheduler/Validators.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace SimpleSheduler
 {
     public static class Validators
     {
-        public static bool Validate_SheduledTaskList(List<Dictionary<string, string>> taskList)
+        public static HashSet<string> Validate_SheduledTaskList(List<Dictionary<string, string>> taskList)
         {
+            HashSet<string> check = new HashSet<string>();
             foreach (var item in taskList)
             {
-                foreach(var key in item) 
-                { 
-                    switch (key.Key) 
+                foreach(var keyValuePair in item)
+                {
+                    switch (keyValuePair.Key) 
                     {
-                        case "":
+                        case "TaskName":
+                            if (string.IsNullOrWhiteSpace(keyValuePair.Value)) check.Add(keyValuePair.Key);
+                            break;
+                        case "IsFinished":
+                        case "IsInProcess":
+                            if (string.IsNullOrWhiteSpace(keyValuePair.Value)) check.Add(keyValuePair.Key);
+                            if (Convert.ToInt32(keyValuePair.Value) > 1 || Convert.ToInt32(keyValuePair.Value) < 0) check.Add(keyValuePair.Key);
+                            break;
+                        case "CreatedDate":
+                            if(!DateTime.TryParse(keyValuePair.Value, out _)) check.Add(keyValuePair.Key);
+                            break;
+                        case "ChangedDate":
+                            if (!string.IsNullOrWhiteSpace(keyValuePair.Value))
+                            {
+                                if(!DateTime.TryParse(keyValuePair.Value, out _)) check.Add(keyValuePair.Key);
+                            }
                             break;
                         default:
                             break;
@@ -24,7 +41,7 @@ namespace SimpleSheduler
                 }
             }
 
-            return true;
+            return check;
         }
     }
 }
